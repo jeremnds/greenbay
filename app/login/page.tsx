e6374f835e18413fa2bb0e31a0d4f55a0ee7de14
jeremnds <jrmymnds@gmail.com>
2024-08-08 22:21:12 +0200

    feat:finished login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,24 @@
+import Image from "next/image";
 import MaxWidthWrapper from "../_components/ui/MaxWidthWrapper";
 import { signInAction } from "../_lib/actions";
 
 export default function page() {
   return (
     <MaxWidthWrapper>
-      <form
-        action={signInAction}
-        className="h-screen flex justify-center items-center"
-      >
-        <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
-          <img
-            src="https://authjs.dev/img/providers/google.svg"
-            alt="Google logo"
-            height="24"
-            width="24"
-          />
-          <span>Continue with Google</span>
-        </button>
-      </form>
+      <div className="h-screen flex justify-center items-center flex-col gap-20">
+        <h3 className="text-3xl">Hey, let&apos;s sign in 👋</h3>
+        <form action={signInAction}>
+          <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
+            <Image
+              src="https://authjs.dev/img/providers/google.svg"
+              alt="Google logo"
+              height="24"
+              width="24"
+            />
+            <span>Continue with Google</span>
+          </button>
+        </form>
+      </div>
     </MaxWidthWrapper>
   );
 }
