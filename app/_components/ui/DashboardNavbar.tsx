b1c7ccd8945dcc9557f45a344ded0e5f329f3aa8
jeremnds <jrmymnds@gmail.com>
2024-08-09 16:11:23 +0200

    feat: adding pages and typing dashboard navbar

--- app/_components/ui/DashboardNavbar.tsx
+++ app/_components/ui/DashboardNavbar.tsx
@@ -1,52 +1,42 @@
 "use client";
-
+import { navLinks } from "@/app/_lib/constants";
 import { cn } from "@/app/_lib/utils";
 import { BookOpen, House, ShoppingBasket, User } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+type NavLinkName = "Home" | "Products" | "Categories" | "Users";
+
+const iconMap: Record<NavLinkName, React.ComponentType> = {
+  Home: House,
+  Products: ShoppingBasket,
+  Categories: BookOpen,
+  Users: User,
+};
+
 export default function DashboardNavbar() {
-  const navLinks = [
-    {
-      name: "Home",
-      href: "/dashboard",
-      icon: <House />,
-    },
-    {
-      name: "Products",
-      href: "/dashboard/products",
-      icon: <ShoppingBasket />,
-    },
-    {
-      name: "Categories",
-      href: "/dashboard/categories",
-      icon: <BookOpen />,
-    },
-    {
-      name: "Users",
-      href: "/dashboard/users",
-      icon: <User />,
-    },
-  ];
   const pathName = usePathname();
   return (
     <div className="border-r-2 border-gray-20000 h-screen">
       <div className="">
         <ul className="text-black flex flex-col gap-6 md:gap-2 items-center md:items-start ">
-          {navLinks.map((link) => (
-            <li key={link.name} className="md:w-full ">
-              <Link
-                href={link.href}
-                className={cn(
-                  "hover:text-green-600 flex gap-6 items-center md:px-6 md:py-4 md:hover:bg-secondary",
-                  { "md:bg-secondary text-green-600": link.href === pathName }
-                )}
-              >
-                {link.icon}
-                <span className="hidden md:block">{link.name}</span>
-              </Link>
-            </li>
-          ))}
+          {navLinks.map((link) => {
+            const Icon = iconMap[link.name as NavLinkName];
+            return (
+              <li key={link.name} className="md:w-full ">
+                <Link
+                  href={link.href}
+                  className={cn(
+                    "hover:text-green-600 flex gap-6 items-center md:px-6 md:py-4 md:hover:bg-secondary",
+                    { "md:bg-secondary text-green-600": link.href === pathName }
+                  )}
+                >
+                  <Icon />
+                  <span className="hidden md:block">{link.name}</span>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
---
+++ app/_lib/constants.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { name: "Home", href: "/dashboard" },
+  { name: "Products", href: "/dashboard/products" },
+  { name: "Categories", href: "/dashboard/categories" },
+  { name: "Users", href: "/dashboard/users" },
+];
