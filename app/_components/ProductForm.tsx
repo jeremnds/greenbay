8de4553b266fb34fb40ffe03bd6c83359508c9a7
jeremnds <jrmymnds@gmail.com>
2024-08-10 14:52:22 +0200

    feat: completing form for product

--- app/_components/ProductForm.tsx
+++ app/_components/ProductForm.tsx
@@ -1,6 +1,15 @@
 import { CategoriesType, ProductType } from "../_models/types";
 import { Input } from "./ui/shadcn/input";
 import { Label } from "./ui/shadcn/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/shadcn/select";
 import { Textarea } from "./ui/shadcn/textarea";
 
 type ProductFormProps = {
@@ -9,6 +18,12 @@ type ProductFormProps = {
 };
 
 export default function ProductForm({ product, categories }: ProductFormProps) {
+  const productCategory = categories.filter(
+    (category) => category.id === product.category_id
+  );
+  const categoryName =
+    productCategory.length > 0 ? productCategory[0].name : undefined;
+
   return (
     <form action="">
       <div className="grid  w-full max-w-sm items-center gap-1.5">
@@ -26,6 +41,36 @@ export default function ProductForm({ product, categories }: ProductFormProps) {
           name="description"
           defaultValue={product.description}
         />
+
+        <div className="flex gap-4">
+          <div>
+            <Label htmlFor="category">Category</Label>
+            <Select defaultValue={categoryName} name="category">
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select a category" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Categories</SelectLabel>
+                  {categories.map((category) => (
+                    <SelectItem key={category.id} value={category.name}>
+                      {category.name}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <div>
+            <Label htmlFor="price">Price</Label>
+            <Input
+              type="number"
+              id="price"
+              name="price"
+              defaultValue={product.price}
+            />
+          </div>
+        </div>
       </div>
     </form>
   );
