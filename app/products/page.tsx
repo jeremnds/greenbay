de25a3aa3bb7804d7cdda0cb2d19bf67d13572ce
jeremnds <jrmymnds@gmail.com>
2024-08-12 15:32:57 +0200

    feat: add search bar

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,10 @@
 import PageContainer from "@/src/components/atoms/PageContainer";
+import SearchBar from "@/src/components/molecules/SearchBar";
 
 export default function page() {
-  return <PageContainer>Product page</PageContainer>;
+  return (
+    <PageContainer>
+      <SearchBar />
+    </PageContainer>
+  );
 }
