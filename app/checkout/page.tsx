fc3d1df2b1b56cd1448284fa5957b78bafb82a99
jeremnds <jrmymnds@gmail.com>
2024-08-15 16:36:49 +0200

    feat: add stripe colors according to theme

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -6,6 +6,7 @@ import { convertToSubcurrency } from "@/src/lib/utils";
 import { useCartStore } from "@/src/store/cartStore";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import { useTheme } from "next-themes";
 if (process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY === undefined) {
   throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is undefined");
 }
@@ -19,6 +20,9 @@ export default function Page() {
 
   const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
 
+  const { theme, systemTheme } = useTheme();
+  const resolvedTheme = theme === "system" ? systemTheme : theme;
+
   return (
     <PageContainer>
       <Elements
@@ -27,6 +31,16 @@ export default function Page() {
           mode: "payment",
           amount: convertToSubcurrency(total),
           currency: "usd",
+          appearance: {
+            theme:
+              theme === "dark"
+                ? "night"
+                : theme === "system"
+                ? systemTheme === "dark"
+                  ? "night"
+                  : "stripe"
+                : "stripe",
+          },
         }}
       >
         <Checkout total={total} />
