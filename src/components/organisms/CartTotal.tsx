24575b28e5396993fe8185c89e4d79b8337cab98
jeremnds <jrmymnds@gmail.com>
2024-08-15 11:35:18 +0200

    feat: add clear items on cart

--- src/components/organisms/CartTotal.tsx
+++ src/components/organisms/CartTotal.tsx
@@ -1,19 +1,30 @@
+import { useCartStore } from "@/src/store/cartStore";
+import { Button } from "../atoms/Button";
+
 type CartTotalProps = {
   totalPrice: number | null;
 };
 
 export default function CartTotal({ totalPrice }: CartTotalProps) {
+  const clearCart = useCartStore((state) => state.clearCart);
   return (
     <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
-      <div className="w-screen max-w-lg space-y-4">
+      <div className="w-screen  space-y-4">
         <dl className="space-y-0.5 text-sm text-gray-700 dark:text-gray-200">
-          <div className="flex justify-between !text-base font-medium">
+          <div className="flex justify-between ml-auto max-w-lg !text-base font-medium">
             <dt>Total</dt>
             <dd>${totalPrice}</dd>
           </div>
         </dl>
 
-        <div className="flex justify-end">
+        <div className="flex justify-between items-center">
+          <Button
+            variant="destructive"
+            className="px-5 py-6"
+            onClick={clearCart}
+          >
+            Clear cart
+          </Button>
           <a
             href="#"
             className="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
